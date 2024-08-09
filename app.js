53cb7aadb26df1c7560d9365fb1c550e8ab4c9c7
Thong Dino <hodinhthong.auto@gmail.com>
2024-08-09 15:23:48 +0700

    setup to connect with database

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+require('dotenv').config()
 
+const express = require('express')
 
+const sequelize = require('./db/database')
+const app = express()
 
+sequelize
+    .sync({ alter: true })
+    
+    .then(() => {
+        
+        console.log('Table and model synced successfully');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+module.exports = app
