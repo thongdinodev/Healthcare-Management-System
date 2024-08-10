31a5aa72ef568788e01133d13d070941701899ea
Thong Dino <hodinhthong.auto@gmail.com>
2024-08-10 10:24:19 +0700

    setup initially validation file in utils

---
+++ utils/validation.js
@@ -0,0 +1,52 @@
+const Joi = require('joi')
+
+const patientValidate = data => {
+    const patientSchema = Joi.object({
+        first_name: Joi.string().required(),
+        last_name: Joi.string().required(),
+        date_of_birth: Joi.date().required(),
+        phone_number: Joi.string().required(),
+        address: Joi.string(),
+        gender: Joi.string(),
+        insurance_information: Joi.string().required()
+    })
+
+    return patientSchema.validate(data)
+}
+
+const doctorValidate = data => {
+    const doctorSchema = Joi.object({
+        first_name: Joi.string().required(),
+        last_name: Joi.string().required(),
+        schedule: Joi.string(),
+        specialization: Joi.string().required().min(3).max(25)
+    })
+
+    return doctorSchema.validate(data)
+}
+
+const appointmentValidate = data => {
+    const appointmentSchema = Joi.object({
+        appointment_date: Joi.date().required(),
+        status: Joi.string().required()
+    })
+
+    return appointmentSchema.validate(data)
+}
+
+const billingValidate = data => {
+    const billingSchema = Joi.object({
+        amount: Joi.number().required(),
+        billing_date: Joi.date().required(),
+        payment_status: Joi.string().required()
+    })
+
+    return billingSchema.validate(data)
+}
+
+module.exports = {
+    patientValidate,
+    doctorValidate,
+    appointmentValidate,
+    billingValidate
+}
