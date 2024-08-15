cfa0384328dc15d6cb118f2ea094dbdd63ae4dc8
Thong Dino <hodinhthong.auto@gmail.com>
2024-08-15 11:02:40 +0700

    implement github oauth

--- controllers/OAuth20.Controller.js
+++ controllers/OAuth20.Controller.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const GitHubStrategy = require('passport-github2').Strategy
 
 let userProfile
 
@@ -20,13 +21,13 @@ const getErrorOAuthPage = (req, res, next) => {
     })
 }
 
+// PASSPORT FOR GOOGLE
 
-// PASSPORT SESSION
 const getOAuthGoogle = passport.authenticate('google', { scope : ['profile', 'email'] })
 
 const getOAuthGoogleCallback = passport.authenticate('google', { 
-        failureRedirect: '/error',
-        successRedirect: '/success'
+    failureRedirect: '/error',
+    successRedirect: '/success'
 })
 
 passport.use(new GoogleStrategy({
@@ -36,16 +37,43 @@ passport.use(new GoogleStrategy({
   },
 
     function(accessToken, refreshToken, profile, done) {
+
+        profile.email = profile.emails[0].value
         
         userProfile = profile;
         return done(null, userProfile);
         }
 ))
 
+// PASSPORT FOR GITHUB
+const getOAuthGithub = passport.authenticate('github', { scope : ['user: email'] })
+
+const getOAuthGithubCallback = passport.authenticate('github', { 
+    failureRedirect: '/error',
+    successRedirect: '/success'
+})
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/github/callback"
+  },
+
+    function(accessToken, refreshToken, profile, done) {
+        
+        if (!profile.displayName) profile.displayName = profile.username
+        if (!profile.email) profile.email = profile.profileUrl
+        
+        userProfile = profile;
+        return done(null, userProfile);
+        }
+))
 module.exports = {
     getLoginOAuthPage,
     getErrorOAuthPage,
     getSuccessOAuthPage,
     getOAuthGoogle,
-    getOAuthGoogleCallback
+    getOAuthGoogleCallback,
+    getOAuthGithub,
+    getOAuthGithubCallback
 }
