4bcedd3386506407d2f4807d133c535c5d974ab3
Thong Dino <hodinhthong.auto@gmail.com>
2024-08-18 18:01:24 +0700

    modify to merge handling error dev, prod

--- middlewares/ErrorHandler.js
+++ middlewares/ErrorHandler.js
@@ -4,7 +4,6 @@ const ErrorHandler = (err, req, res, next) => {
     console.log("Middleware Error Handling");
     console.log(err);
     
-    
     err.statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR
 
     const responseError = {
