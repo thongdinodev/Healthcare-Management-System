87954d3f67e57071c64f82d5e727e0af2357de5d
unknown <thong.dino.dev@gmail.com>
2024-09-16 11:39:57 +0700

    fix bug in appointment, modify route versioning for restful api

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,12 +5,12 @@ const router = express.Router()
 router.post('/register', authController.register)
 router.post('/login', authController.loginWithEmailAndPassword)
 router.post('/logout', authController.logoutAccount)
-router.post('/forgotPassword', authController.forgotPassword)
-router.patch('/resetPassword/:token', authController.resetPassword)
+router.post('/forgot-password', authController.forgotPassword)
+router.patch('/reset-password/:token', authController.resetPassword)
 
 // implement protec route
 router.use(authController.protectRoute)
 
-router.patch('/updateMyPassword', authController.updateMyPassword)
+router.patch('/update-password', authController.updateMyPassword)
 
 module.exports = router
